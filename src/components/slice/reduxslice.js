f7ba30a3a478411c86c89630872fe6c99c8267fb
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2024-09-17 14:23:27 +0600

    demo

--- src/components/slice/reduxslice.js
+++ src/components/slice/reduxslice.js
@@ -1,27 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit'
+// import { createSlice } from '@reduxjs/toolkit'
 
-export const reduxslice = createSlice({
-  name: 'counter',
-  initialState: {
-    value: 0,
-  },
-  reducers: {
-    increment: (state) => {
+// export const reduxslice = createSlice({
+//   name: 'counter',
+//   initialState: {
+//     value: 0,
+//   },
+//   reducers: {
+//     increment: (state) => {
       
-      state.value += 1
-    },
-    decrement: (state) => {
-      if(state.value > 1){
-        state.value -= 1
-      }else{
+//       state.value += 1
+//     },
+//     decrement: (state) => {
+//       if(state.value > 1){
+//         state.value -= 1
+//       }else{
 
-      }
+//       }
       
-    },
+//     },
    
-  },
-})
+//   },
+// })
 
-export const { increment, decrement,  } = reduxslice.actions
+// export const { increment, decrement,  } = reduxslice.actions
 
-export default reduxslice.reducer
+// export default reduxslice.reducer
