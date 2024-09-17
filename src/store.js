a3aa90f16ca7e3729b75e19be4d3075ed6b41fea
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2024-09-17 14:05:40 +0600

    demo

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import  reduxslice  from './components/slice/Reduxslice'
+import  Reduxslice  from './components/slice/Reduxslice'
 
 export default configureStore({
   reducer: {
-    counter:reduxslice
+    counter:Reduxslice
   },
 })
