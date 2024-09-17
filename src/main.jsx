1c68c3f4c8357e66f0a833679d4f1bfb0537ef92
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2024-09-17 13:45:19 +0600

    demo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import firebaseConfig from './firebase.Config.js'
-import store from './store'
+import store from './store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
